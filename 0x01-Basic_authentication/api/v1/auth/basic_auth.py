14aa964934fc4dfe3b7e29ef48b3e30e5eb1df02
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-03 01:09:34 +0300

    Adding api/v1/auth/basic_auth.py file and updating api/v1/app.py file

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains BasicAuth class"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Handles the Baisc Auth method to authenticate users"""
+
+    pass
