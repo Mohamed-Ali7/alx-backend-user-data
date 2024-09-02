da565b7f4f5e5603db879086ee5a005eeddee13f
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-03 01:31:25 +0300

    Updating api/v1/auth/basic_auth.py file

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """This module contains BasicAuth class"""
 
+import binascii
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,23 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        Rreturns the decoded value of a Base64
+        string base64_authorization_header:
+        """
+
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decoded_header = base64.b64decode(base64_authorization_header,
+                                              validate=True)
+            return decoded_header.decode("utf-8")
+        except binascii.Error:
+            return None
