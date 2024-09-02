74409bde013eb08edb6d7a3062cdc34ba8646337
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-03 00:24:50 +0300

    Updating api/v1/auth/auth.py file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,20 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns whether the path is protected or not"""
-        return False
+        if not path or not excluded_paths:
+            return True
+
+        if not path.endswith("/"):
+            path = "{}/".format(path)
+
+        for exc_path in excluded_paths:
+            if not exc_path.endswith("/"):
+                exc_path = "{}/".format(exc_path)
+
+            if path == exc_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the Authorization Header"""
