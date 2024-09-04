94417ebf2e735f615c92d987a3ddb32ea7096f54
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-05 01:07:36 +0300

    Updating api/v1/auth/session_auth.py file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,22 @@
 """This module contains SessionAuth class"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Handles the Session Auth method to authenticate users"""
 
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+
+        if not user_id or type(user_id) is not str:
+            return None
+
+        session_id = uuid.uuid4()
+
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
