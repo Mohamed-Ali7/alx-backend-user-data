ff879d482974e276c6e12f97f5dab9fb960ed5ed
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-05 01:34:27 +0300

    Updating api/v1/auth/session_auth.py file

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,7 +16,7 @@ class SessionAuth(Auth):
         if not user_id or type(user_id) is not str:
             return None
 
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
 
         SessionAuth.user_id_by_session_id[session_id] = user_id
 
