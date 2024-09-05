7c6f079f0f42117a90a8887a7c8e5c8a2dcfc4c0
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-05 22:03:49 +0300

    Adding api/v1/auth/session_db_auth.py and models/user_session.py files and updating api/v1/app.py file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,6 +28,9 @@ elif auth_type == "session_auth":
 elif auth_type == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
@@ -78,4 +81,4 @@ def access_forbidden(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
