198b7365166c730cfa09fe22e29cbde60ec3988c
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-13 01:49:23 +0300

    Updating db.py file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,15 +35,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Saves the user to the database and returns it"""
 
-        new_user = User(email=email, hashed_password=hashed_password)
-        try:
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception as e:
-            print(f"Error adding user to database: {e}")
-            self._session.rollback()
-            raise
-        return new_user
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
 
     def find_user_by(self, **kwargs) -> User:
         """
