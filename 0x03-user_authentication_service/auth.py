c8107d445f0f64520c6aa69d86c6edd98288077d
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-13 02:06:56 +0300

    Updating auth.py file (Task 5)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,7 +24,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> User:
+    def register_user(self, email: str, password: str):
         """Registers a new user"""
 
         try:
