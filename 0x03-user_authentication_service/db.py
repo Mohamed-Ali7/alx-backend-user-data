24bcbc1ad11750390f4e644c4a02f30dc9c0816e
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-12 23:30:01 +0300

    Updating db.py file (Task 2)

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -39,5 +40,22 @@ class DB:
         self._session.add(user)
 
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Returns the first row found in the users table
+        as filtered by the method’s input arguments
+        """
+
+        for key in kwargs:
+            if key not in User.__dict__:
+                raise InvalidRequestError()
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound()
 
         return user
