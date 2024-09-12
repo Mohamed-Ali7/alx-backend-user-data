8fa2e09b640c164160449c3b5ec43ee03562768d
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-13 02:09:27 +0300

    Updating auth.py file (Task 5)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,12 +24,12 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """Registers a new user"""
 
         try:
             self._db.find_user_by(email=email)
-            ValueError("User {} already exists".format(email))
+            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             pass
 
