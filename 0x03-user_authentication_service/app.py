64abd8fabe3fb23d8f41539e4cde7b12115874a6
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-13 04:54:26 +0300

    Updating app.py file (Task 15)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -97,5 +97,27 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """
+    Generates a password reset token and returns it for
+    a user with a specific email
+    """
+
+    user_payload = request.form
+
+    if user_payload is None:
+        abort(400, description="Not a JSON")
+
+    email = user_payload.get("email")
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
