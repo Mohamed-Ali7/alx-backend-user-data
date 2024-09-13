da3b5bc649f5bf99a648e5fd7e4145aa8057d7ad
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-13 04:05:04 +0300

    Updating auth.py file (Task 13)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -98,10 +98,7 @@ class Auth:
         (Logs the user out)
         """
 
-        if not user_id:
-            return None
-
         try:
-            self._db.update_user(user_id, {"session_id": None})
+            self._db.update_user(user_id, session_id=None)
         except Exception:
             return None
