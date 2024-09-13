23e7deeb5bfaf11aef8b1b67cb041c5fa1cdc6d4
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-09-13 03:01:23 +0300

    Updating app.py file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 
 """This modules starts a flask app"""
 
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -13,6 +15,26 @@ def index():
     """returns a simple json response"""
 
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """API to register a new user"""
+
+    user_payload = request.form
+
+    if user_payload is None:
+        abort(400, description="Not a JSON")
+
+    email = user_payload.get('email')
+    password = user_payload.get('password')
+
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
